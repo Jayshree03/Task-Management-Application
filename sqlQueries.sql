3f7c4708acb59fb7af25e61a200802e5037e56ed
Jayshree03 <jayshreegupta1020@gmail.com>
2024-02-11 16:35:23 +0530

    Create sqlQueries.sql
    sqlQueries Added

---
+++ sqlQueries.sql
@@ -0,0 +1,9 @@
+-- create database taskmanagement;
+-- use taskmanagement;
+/*create table task(
+	taskid int auto_increment not null primary key,
+	newtask varchar(50),
+    deadline date,
+    priority varchar(20), 
+    descrip varchar(100)
+)*/

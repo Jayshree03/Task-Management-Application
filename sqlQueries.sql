97976af567441b4ac8e930c7e63aa73209fd3b18
Jayshree03 <jayshreegupta1020@gmail.com>
2024-02-11 17:09:42 +0530

    script edited sql updated
    script edited
    sql updated

--- sqlQueries.sql
+++ sqlQueries.sql
@@ -7,3 +7,5 @@
     priority varchar(20), 
     descrip varchar(100)
 )*/
+-- insert into task(newtask, deadline, priority, descrip) values ("Update student record", "2024/4/1", "High Priority", "Update the existing records of students by removing ex-student");
+-- select * from task;

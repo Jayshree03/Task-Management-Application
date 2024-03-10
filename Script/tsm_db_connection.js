e397482e9546073f69fddf7067101df345569d5a
Jayshree03 <jayshreegupta1020@gmail.com>
2024-03-10 12:53:43 +0530

    Revert "Revert "node updation""
    This reverts commit 7a0e5ad40661f066d2d2339bd83c2e96f266fd8b.

--- Script/tsm_db_connection.js
+++ Script/tsm_db_connection.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const app = express();
+
+
 const mysql = require("mysql");
 
-app.get('/', function(req, res){
-    const config ={
-        user: 'root',
-        password: 'root',
-        server: 'localhost',
-        database: 'taskmanagement'
-    };
-    mysql.createConnection(config, function(err){
-        let request = new mysql.Request();
-        request.query('insert into task(newtask, deadline, priority, descrip) values ("Update student record", "2024-4-1", "High Priority", "Update the existing records of students by removing ex-student',
-        function(err, records){
-            if(err) console.log(err)
-            res.send(records);
-        });
-    });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'taskmanagement'
 });
-let server = app.listen(5000, function () {
-    console.log('Server is listening at port 5000...');
+  
+connection.connect((error) => {
+    if (error) {
+      console.error('Error connecting to MySQL database:', error);
+    } else {
+      console.log('Connected to MySQL database!');
+    }
 });
+
+    
+
+
